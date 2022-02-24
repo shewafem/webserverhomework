b6e99848cfa626685b3ffad349cf5195c16da3bd
tamir <tamirsundureev@gmail.com>
2022-02-25 00:44:37 +0800

    added files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>тестируем PHP!</title>
+</head>
+<body>
+    <header>
+            <img width="500" style="position: relative; left: 50%; transform: translate(-50%);" src="hgh.png" alt="logo-polytech">
+    </header> 
+    <main>
+        <h1 style="text-align: center;">
+        <?php echo "Hello, World!" ?></h1>
+        <div style="text-align:center;"> 
+            <?php 
+                echo "current time is: "; 
+                echo date('m/d/Y h:i:s')."<br>"; 
+                echo "<b>done by Tamir Sundureev, group 211-322</b>";
+            ?> 
+        </div>
+    </main>
+    <footer>
+        <h2 style="text-align:center; font-family: 'Courier New';">Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. Залить на хостинг. 
+</h2>
+    </footer>
+</body>
+</html>
